bdab409fdc222244876a855593e19223ce28ab6f
Neal JIn Oh <no4259@gmail.com>
2019-09-23 16:21:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
-# code here!
+class School
+  
+  def initialization(school, roster)
+    @school = school
+    @roster = roster
+  end
+  
+  def add_student
+  end
+  
+  
+end
+
+# school = School.new("Bayside High School")
